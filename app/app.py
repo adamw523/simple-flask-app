c3bee2e6666a8c27087d42877a5c850400574fdc
Adam Wisniewski <adamw@tbcn.ca>
2016-09-13 15:01:39 +0000

    adding debug route

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, current_app
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "hi there"
+
+@app.route('/debug')
+def debug():
+    assert current_app.debug == False
